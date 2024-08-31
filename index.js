64c57b96e244efcf7b440ea4251bd91d45c11461
Afroze Pathan <afrozkhan.uak@gmail.com>
2024-08-31 13:59:19 +0530

    ok

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 
 
 // const dburl = "mongodb+srv://afrozkhanuak:kSMTeKwFukKdZuEC@cluster0.mh481zy.mongodb.net/Scaler?retryWrites=true&w=majority&appName=Cluster0"
-const dburl = "mongodb+srv://afrozkhanuak:kSMTeKwFukKdZuEC@cluster0.mh481zy.mongodb.net/"
+const dburl = "mongodb+srv://afrozkhanuak:eWxZL0dCGo8raXVx@cluster0.mh481zy.mongodb.net/Scaler?retryWrites=true&w=majority&appName=Cluster0"
 
 // mongodb+srv://karankumar254:6EwHq9Y4tatrVXon@cluster0.buymhu8.mongodb.net/
 
